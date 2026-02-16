5e4d61e1ce4c4f7cff42017f054623ad87bbfd2a
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-16 17:44:55 +0100

    drm/amd/display: Check for VRR range in CEA AMD vsdb
    [Why]
    Some monitors only expose GTF ranges (or others, without Range Limits
    Only flag). This breaks VRR even though they have explicit FreeSync
    support.
    
    Currently, if monitor ranges were missing, amdgpu only searched for AMD
    vsdb in DisplayID but many monitors have it in CEA, just like HDMI.
    
    [How]
    For DP and eDP connections, check for VRR ranges provided in AMD vendor-
    specific data block if VRR range wasn't detected.
    
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/3894
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4457
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4747
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4856
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -13077,6 +13077,12 @@ void amdgpu_dm_update_freesync_caps(struct drm_connector *connector,
 		/* Some eDP panels only have the refresh rate range info in DisplayID */
 		if (is_monitor_range_invalid(connector))
 			parse_edid_displayid_vrr(connector, edid);
+		/*
+		 * Many monitors expose AMD vsdb in CAE even for DP and their
+		 * monitor ranges do not contain Range Limits Only flag
+		 */
+		if (is_monitor_range_invalid(connector))
+			monitor_range_from_vsdb(&connector->display_info, &vsdb_info);
 
 		if (dpcd_caps.allow_invalid_MSA_timing_param)
 			freesync_capable = copy_range_to_amdgpu_connector(connector);
