d6c60f2267e74f7773ed1593eb6f85e6f5861c5b
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-16 17:45:06 +0100

    drm/amd/display: Save HDMI gaming info to edid caps
    [Why]
    We need info about these features in parts of the driver where fishing
    for drm_connector struct is infeasible.
    
    [How]
    Add three new fields to dc_edid_caps and fill them if connected device
    is HDMI based on it's EDID
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -136,7 +136,12 @@ enum dc_edid_status dm_helpers_parse_edid_caps(
 				  edid_caps->display_name,
 				  AUDIO_INFO_DISPLAY_NAME_SIZE_IN_CHARS);
 
-	edid_caps->edid_hdmi = connector->display_info.is_hdmi;
+	if (connector->display_info.is_hdmi) {
+		edid_caps->edid_hdmi = true;
+		edid_caps->allm = connector->display_info.hdmi.allm;
+		edid_caps->fva = connector->display_info.hdmi.vrr_cap.fva;
+		edid_caps->hdmi_vrr = connector->display_info.hdmi.vrr_cap.supported;
+	}
 
 	apply_edid_quirks(dev, edid_buf, edid_caps);
 
