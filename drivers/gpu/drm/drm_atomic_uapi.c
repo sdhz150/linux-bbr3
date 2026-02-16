009df33d2134784aab635b08055b9e533a05c334
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-16 17:45:12 +0100

    drm: Add passive_vrr_disabled property to crtc
    Many TVs and other HDMI sinks suffer from blanking and possibly other
    glitches when VRR is toggled. With VRR present on such sinks and
    vrr_on_desktop enabled, they behave like the signal is always variable,
    even in fixed refresh rate situations. DisplayPort and eDP enforce
    seamless VRR transitions but HDMI unfortunately doesn't.
    
    Keep VRR toggled if it's supported and not explicitly disabled. It can
    be used for any VRR sinks, but this is mainly targeted for HDMI.
    
    Functionally, for an end user, this is the same as normal, fixed refresh
    rate mode. The only difference is that sink is kept in VRR state which
    enables seamless transitions into/out of variable refresh rate.
    
    Basically, the driver shouldn't change it's behavior around VRR_ENABLED
    set to false, jut keep sending info packets/frames with VRR/FreeSync/
    G-Sync/HDMI VRR active.
    
    Enabled by default for sinks that claim it's support
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/gpu/drm/drm_atomic_uapi.c
+++ drivers/gpu/drm/drm_atomic_uapi.c
@@ -383,6 +383,8 @@ static int drm_atomic_crtc_set_property(struct drm_crtc *crtc,
 		return ret;
 	} else if (property == config->prop_vrr_enabled) {
 		state->vrr_enabled = val;
+	} else if (property == config->prop_passive_vrr_disabled) {
+		state->passive_vrr_disabled = val;
 	} else if (property == config->degamma_lut_property) {
 		ret = drm_property_replace_blob_from_id(dev,
 					&state->degamma_lut,
@@ -446,6 +448,8 @@ drm_atomic_crtc_get_property(struct drm_crtc *crtc,
 		*val = (state->mode_blob) ? state->mode_blob->base.id : 0;
 	else if (property == config->prop_vrr_enabled)
 		*val = state->vrr_enabled;
+	else if (property == config->prop_passive_vrr_disabled)
+		*val = state->passive_vrr_disabled;
 	else if (property == config->degamma_lut_property)
 		*val = (state->degamma_lut) ? state->degamma_lut->base.id : 0;
 	else if (property == config->ctm_property)
